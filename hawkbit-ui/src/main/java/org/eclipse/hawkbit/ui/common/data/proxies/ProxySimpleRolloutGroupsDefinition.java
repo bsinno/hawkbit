af951eecbbd44a3c217a08f4a5150fa8b510f12c
Kumar Anand (INST/ESY3) <Anand.Kumar@bosch-si.com>
2020-06-23 14:06:26 +0200

    Added java docs and updated license header in ui-proxies Signed-off-by: Anand Kumar <anand.kumar@bosch-si.com>

--- hawkbit-ui/src/main/java/org/eclipse/hawkbit/ui/common/data/proxies/ProxySimpleRolloutGroupsDefinition.java
+++ hawkbit-ui/src/main/java/org/eclipse/hawkbit/ui/common/data/proxies/ProxySimpleRolloutGroupsDefinition.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2019 Bosch Software Innovations GmbH and others.
+ * Copyright (c) Copyright (c) 2020 Bosch.IO GmbH and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -21,26 +21,59 @@ public class ProxySimpleRolloutGroupsDefinition implements Serializable {
     private String triggerThresholdPercentage;
     private String errorThresholdPercentage;
 
+    /**
+     * Gets the count of simple rollout Groups
+     *
+     * @return numberOfGroups
+     */
     public Integer getNumberOfGroups() {
         return numberOfGroups;
     }
 
+    /**
+     * Sets the numberOfGroups
+     *
+     * @param numberOfGroups
+     *           count of simple rollout Groups
+     */
     public void setNumberOfGroups(final Integer numberOfGroups) {
         this.numberOfGroups = numberOfGroups;
     }
 
+    /**
+     * Gets the percentage of triggerThreshold
+     *
+     * @return triggerThresholdPercentage
+     */
     public String getTriggerThresholdPercentage() {
         return triggerThresholdPercentage;
     }
 
+    /**
+     * Sets the triggerThresholdPercentage
+     *
+     * @param triggerThresholdPercentage
+     *          percentage of triggerThreshold
+     */
     public void setTriggerThresholdPercentage(final String triggerThresholdPercentage) {
         this.triggerThresholdPercentage = triggerThresholdPercentage;
     }
 
+    /**
+     * Gets the percentage of errorThreshold
+     *
+     * @return errorThresholdPercentage
+     */
     public String getErrorThresholdPercentage() {
         return errorThresholdPercentage;
     }
 
+    /**
+     * Sets the errorThresholdPercentage
+     *
+     * @param errorThresholdPercentage
+     *          percentage of errorThreshold
+     */
     public void setErrorThresholdPercentage(final String errorThresholdPercentage) {
         this.errorThresholdPercentage = errorThresholdPercentage;
     }
--- hawkbit-ui/src/main/java/org/eclipse/hawkbit/ui/common/data/proxies/ProxyUploadProgress.java
+++ hawkbit-ui/src/main/java/org/eclipse/hawkbit/ui/common/data/proxies/ProxyUploadProgress.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2019 Bosch Software Innovations GmbH and others.
+ * Copyright (c) Copyright (c) 2020 Bosch.IO GmbH and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -23,42 +23,92 @@ public class ProxyUploadProgress extends ProxyIdentifiableEntity {
     private double progress;
     private String reason;
 
+    /**
+     * Constructor for ProxyUploadProgress
+     */
     public ProxyUploadProgress() {
         setId(new SecureRandom().nextLong());
     }
 
+    /**
+     * Gets the file upload status
+     *
+     * @return status
+     */
     public ProgressSatus getStatus() {
         return status;
     }
 
+    /**
+     * Sets the status
+     *
+     * @param status
+     *         file upload status
+     */
     public void setStatus(final ProgressSatus status) {
         this.status = status;
     }
 
+    /**
+     * Gets the file upload progress
+     *
+     * @return progress
+     */
     public double getProgress() {
         return progress;
     }
 
+    /**
+     * Sets the progress
+     *
+     * @param progress
+     *         file upload progress
+     */
     public void setProgress(final double progress) {
         this.progress = progress;
     }
 
+    /**
+     * Gets the file upload reason
+     *
+     * @return reason
+     */
     public String getReason() {
         return reason;
     }
 
+    /**
+     * Sets the reason
+     *
+     * @param reason
+     *         file upload reason
+     */
     public void setReason(final String reason) {
         this.reason = reason;
     }
 
+    /**
+     * Gets the fileUploadId
+     *
+     * @return fileUploadId
+     */
     public FileUploadId getFileUploadId() {
         return fileUploadId;
     }
 
+    /**
+     * Sets the fileUploadId
+     *
+     * @param fileUploadId
+     *         Id of fileUpload process
+     */
     public void setFileUploadId(final FileUploadId fileUploadId) {
         this.fileUploadId = fileUploadId;
     }
 
+    /**
+     * Status of the file upload process
+     */
     public enum ProgressSatus {
         INPROGRESS, FINISHED, FAILED;
     }
