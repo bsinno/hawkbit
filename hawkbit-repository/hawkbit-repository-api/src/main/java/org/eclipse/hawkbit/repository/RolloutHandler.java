7f6c766a0ab16d197a7f37f1d6c7fe8417160135
Ahmed Sayed <ahmed.sayed@bosch.io>
2021-03-03 14:09:10 +0100

    added documentation
    Signed-off-by: Ahmed Sayed <ahmed.sayed@bosch.io>

--- hawkbit-repository/hawkbit-repository-api/src/main/java/org/eclipse/hawkbit/repository/RolloutHandler.java
+++ hawkbit-repository/hawkbit-repository-api/src/main/java/org/eclipse/hawkbit/repository/RolloutHandler.java
@@ -8,10 +8,43 @@
  */
 package org.eclipse.hawkbit.repository;
 
+import static org.eclipse.hawkbit.im.authentication.SpPermission.SpringEvalExpressions;
+
+import org.eclipse.hawkbit.repository.model.Target;
+import org.springframework.security.access.prepost.PreAuthorize;
+
 /**
  * Represents the handler service for creating, deleting, and starting a Rollout
  */
 @FunctionalInterface
 public interface RolloutHandler {
+
+    /**
+     * Process rollout based on its current {@link Rollout#getStatus()}.
+     *
+     * For {@link RolloutStatus#CREATING} that means creating the
+     * {@link RolloutGroup}s with {@link Target}s and when finished switch to
+     * {@link RolloutStatus#READY}.
+     *
+     * For {@link RolloutStatus#READY} that means switching to
+     * {@link RolloutStatus#STARTING} if the {@link Rollout#getStartAt()} is set
+     * and time of calling this method is beyond this point in time. This auto
+     * start mechanism is optional. Call {@link #start(Long)} otherwise.
+     *
+     * For {@link RolloutStatus#STARTING} that means starting the first
+     * {@link RolloutGroup}s in line and when finished switch to
+     * {@link RolloutStatus#RUNNING}.
+     *
+     * For {@link RolloutStatus#RUNNING} that means checking to activate further
+     * groups based on the defined thresholds. Switched to
+     * {@link RolloutStatus#FINISHED} is all groups are finished.
+     *
+     * For {@link RolloutStatus#DELETING} that means either soft delete in case
+     * rollout was already {@link RolloutStatus#RUNNING} which results in status
+     * change {@link RolloutStatus#DELETED} or hard delete from the persistence
+     * otherwise.
+     *
+     */
+    @PreAuthorize(SpringEvalExpressions.IS_SYSTEM_CODE)
     void handle(long rolloutId);
 }
